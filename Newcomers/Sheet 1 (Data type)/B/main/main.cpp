06c5a9abd17567a93720c5b26af6b31e594c60da
Mohamed Elsisi <mohammedelsisi21@gmail.com>
2024-09-04 13:21:53 +0300

    add file

--- Newcomers/Sheet 1 (Data type)/B/main/main.cpp
+++ Newcomers/Sheet 1 (Data type)/B/main/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+
+int main() {
+    int int_x;
+    long long long_x;
+    char char_x;
+    float float_x;
+    double double_x;
+    cin>>int_x>>long_x>>char_x>>float_x>>double_x;
+    cout << int_x <<endl<<long_x<<endl<<char_x<<endl<<float_x<<endl<<double_x<<endl;
     return 0;
 }
