7cc43e9d293033b0dd84adc4d438530eb2bdbbce
Mohamed Elsisi <mohammedelsisi21@gmail.com>
2024-09-05 04:41:39 +0300

    add file

--- Newcomers/Sheet 1 (Data type)/H/main/main.cpp
+++ Newcomers/Sheet 1 (Data type)/H/main/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    double x, y, result;
+    cin>>x>>y;
+    result =x / y;
+    cout<<"floor " <<x<<" / "<<y<<" = "<<floor(result)<<endl;
+    cout<<"ceil " <<x<<" / "<<y<<" = "<<ceil(result)<<endl;
+    cout<<"round " <<x<<" / "<<y<<" = "<<round(result)<<endl;
     return 0;
 }
