782ec596da636d0caf00d3ab9e4e84f9db1ab419
Mohamed Elsisi <mohammedelsisi21@gmail.com>
2024-09-08 15:38:59 +0300

    added a file

--- Newcomers/Sheet 1 (Data type)/contest/H/main/main.cpp
+++ Newcomers/Sheet 1 (Data type)/contest/H/main/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+    long long n, m, k ,res = 0, test1, test2, test3;
+    cin>>n>>m>>k;
+    test1 = min(n, min(m, k));
+    res += test1;
+    n -=test1;
+    m -=test1;
+    k -=test1;
+
+    test2 = min(n / 2 , min(m , k));
+    res += test2;
+    n -=test2;
+    m -=test2;
+    k -=test2;
+
+    test3 = min(n/2 , k);
+    res +=test3;
+
+    cout<<res<<endl;
     return 0;
 }
--- Newcomers/Sheet 1 (Data type)/contest/I/main/main.cpp
+++ Newcomers/Sheet 1 (Data type)/contest/I/main/main.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int a, one, two;
+    cin>>a;
+    one = a % 10;
+    two = a / 10;
+    if(one % two == 0 || two % one == 0 || a % 10 == 0)
+        cout<<"YES"<<endl;
+    else
+        cout<<"NO"<<endl;
     return 0;
 }
